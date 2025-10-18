1728fd28f35e6d5eae2f5ea85e11eba07f58ed09
tarekmassoud <t.massoud@hotmail.com>
2025-10-18 13:50:16 +0300

    fix: single HashRouter to avoid basename/path issues on Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // <-- HashRouter
 import Index from "./pages/Index";
 import AllRecipes from "./pages/AllRecipes";
 import RecipeDetail from "./pages/RecipeDetail";
@@ -15,8 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* IMPORTANT: basename so routes work under /cbb on GitHub Pages */}
-      <BrowserRouter basename="/cbb">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/recipes" element={<AllRecipes />} />
@@ -24,7 +23,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
